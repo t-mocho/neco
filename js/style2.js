94065618e287343646e3a748369140da0967dd6b
m-mochi <taniyasonn@gmail.com>
2020-01-27 17:58:35 +0900

    レスポンシヴ

--- js/style2.js
+++ js/style2.js
@@ -1,46 +1,76 @@
-// ナビゲーション固定
-$(function() {
-  var navHeight = 50;
-  var $win = $(window),
-    $main = $('main'),
-    $nav = $('nav'),
-    navHeight = $nav.outerHeight(),
-    navPos = $nav.offset().top,
-    fixedClass = 'is-fixed';
+jQuery(document).ready(function($) {
+  //560px以上の場合
+  if (window.matchMedia('(min-width: 560px)').matches) {
+    // ヘッダー途中固定
+    $(function() {
+      var navHeight = 50;
+      var $win = $(window),
+        $main = $('main'),
+        $nav = $('nav'),
+        navHeight = $nav.outerHeight(),
+        navPos = $nav.offset().top,
+        fixedClass = 'is-fixed';
 
-  $win.on('load scroll', function() {
-    var value = $(this).scrollTop();
-    if (value > navPos) {
-      $nav.addClass(fixedClass);
-      $main.css('margin-top', navHeight);
-    } else {
-      $nav.removeClass(fixedClass);
-      $main.css('margin-top', '0');
-    }
-  });
+      $win.on('load scroll', function() {
+        var value = $(this).scrollTop();
+        if (value > navPos) {
+          $nav.addClass(fixedClass);
+          $main.css('margin-top', navHeight);
+        } else {
+          $nav.removeClass(fixedClass);
+          $main.css('margin-top', '0');
+        }
+      });
+
+      // スクロール位置
+      $('a[href^="#"]').click(function() {
+        var speed = 550; // ミリ秒
+        var href = $(this).attr("href");
+        var target = $(href == "#" || href == "" ? 'html' : href);
+        if ($('.is-fixed').length) {
+          var headerHeight = 50;
+        } else {
+          var headerHeight = 110;
+        }
+        var position = target.offset().top - headerHeight;
+        $('body,html').animate({
+          scrollTop: position
+        }, speed, 'swing');
+        return false;
+      });
+    });
+    //560px以下の場合
+  } else {
+    $(function() {
+
+      // ハンバーガーメニューアクティブ
+      $(function() {
+        $('#mobile_menu').click(function() {
+          $(this).toggleClass('active');
+
+          if ($(this).hasClass('active')) {
+            $('.mobile_menu-burger').addClass('active'); //クラスを付与
+            $('#menu').slideDown('active');
+          } else {
+            $('.mobile_menu-burger').removeClass('active'); //クラスを外す
+            $('#menu').slideUp('active');
+          }
+        });
+      });
+
+      // リンク押下でメニューを消す
+      $(function() {
+        $('#menu li a').click(function() {
+          $('.mobile_menu-burger').removeClass('active'); //クラスを外す
+          $('#menu').slideUp(600);
+        });
+      });
+
+    });
+  };
 });
 
-// スムーズスクロール
-$(function() {
-  // #で始まるアンカーをクリックした場合に処理
-  $('a[href^="#"]').click(function() {
-    var speed = 550; // ミリ秒
-    var href = $(this).attr("href");
-    var target = $(href == "#" || href == "" ? 'html' : href);
-    if ($('.is-fixed').length) {
-      var headerHeight = 50;
-    } else {
-      var headerHeight = 100;
-    }
-    var position = target.offset().top - headerHeight;
-    $('body,html').animate({
-      scrollTop: position
-    }, speed, 'swing');
-    return false;
-  });
-});
-
-//
+// 画像スクロールしたらフェードイン
 $(function() {
   // 指定クラス を visibility/hiddenで非表示にするよ
   $('.photo_box li').css('visibility', 'hidden');
@@ -56,8 +86,8 @@ $(function() {
   });
 });
 
-// 画像\
-$(function(){
+// 画像モーダル
+$(function() {
   $(".photo_box img").click(function() {
     $("#graydisplay").html($(this).prop('outerHTML'));
     $("#graydisplay").fadeIn(200);
@@ -68,15 +98,16 @@ $(function(){
 });
 
 // 画像リンク無効
-$(function(){
-	$('.photo_box a').click(function(){
-		return false;
-	})
+$(function() {
+  $('.photo_box a').click(function() {
+    return false;
+  })
 });
 
-// 基本の記述
+/* 基本の記述
 $(document).ready(
   function() {
     $("body").niceScroll();
   }
 );
+*/
